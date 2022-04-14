25422633207bc067bc16d2dba547578a8c264399
Kyalo-kekee <kekekuza@gmail.com>
2022-04-14 11:56:10 +0300

    db connection & security

---
+++ migrations/Version20220414082910.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220414082910 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE MshuleUser (id INT IDENTITY NOT NULL, username NVARCHAR(180) NOT NULL, roles VARCHAR(MAX) NOT NULL, password NVARCHAR(255) NOT NULL, FirstName NVARCHAR(22) NOT NULL, MiddleName NVARCHAR(22), LastName NVARCHAR(22), EmployeeNumber NVARCHAR(122) NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_DEBFCE83F85E0677 ON MshuleUser (username) WHERE username IS NOT NULL');
+        $this->addSql('EXEC sp_addextendedproperty N\'MS_Description\', N\'(DC2Type:json)\', N\'SCHEMA\', \'dbo\', N\'TABLE\', \'MshuleUser\', N\'COLUMN\', roles');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA db_accessadmin');
+        $this->addSql('CREATE SCHEMA db_backupoperator');
+        $this->addSql('CREATE SCHEMA db_datareader');
+        $this->addSql('CREATE SCHEMA db_datawriter');
+        $this->addSql('CREATE SCHEMA db_ddladmin');
+        $this->addSql('CREATE SCHEMA db_denydatareader');
+        $this->addSql('CREATE SCHEMA db_denydatawriter');
+        $this->addSql('CREATE SCHEMA db_owner');
+        $this->addSql('CREATE SCHEMA db_securityadmin');
+        $this->addSql('CREATE SCHEMA dbo');
+        $this->addSql('DROP TABLE MshuleUser');
+    }
+}
---
+++ src/Entity/MshuleUser.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MshuleUserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: MshuleUserRepository::class)]
+class MshuleUser implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $username;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
+
+    #[ORM\Column(type: 'string', length: 22)]
+    private $FirstName;
+
+    #[ORM\Column(type: 'string', length: 22, nullable: true)]
+    private $MiddleName;
+
+    #[ORM\Column(type: 'string', length: 22, nullable: true)]
+    private $LastName;
+
+    #[ORM\Column(type: 'string', length: 122)]
+    private $EmployeeNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->FirstName;
+    }
+
+    public function setFirstName(string $FirstName): self
+    {
+        $this->FirstName = $FirstName;
+
+        return $this;
+    }
+
+    public function getMiddleName(): ?string
+    {
+        return $this->MiddleName;
+    }
+
+    public function setMiddleName(?string $MiddleName): self
+    {
+        $this->MiddleName = $MiddleName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->LastName;
+    }
+
+    public function setLastName(?string $LastName): self
+    {
+        $this->LastName = $LastName;
+
+        return $this;
+    }
+
+    public function getEmployeeNumber(): ?string
+    {
+        return $this->EmployeeNumber;
+    }
+
+    public function setEmployeeNumber(string $EmployeeNumber): self
+    {
+        $this->EmployeeNumber = $EmployeeNumber;
+
+        return $this;
+    }
+}
