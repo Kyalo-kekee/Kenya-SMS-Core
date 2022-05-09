e6703c86d97451a4fddd1cf5665feff0baa673f7
Kyalo-kekee <kekekuza@gmail.com>
2022-05-09 13:24:14 +0300

    menu -update

--- src/Security/UserServerAuthenticator.php
+++ src/Security/UserServerAuthenticator.php
@@ -49,9 +49,7 @@ class UserServerAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        return new RedirectResponse($this->urlGenerator->generate('/'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl(Request $request): string
