11c5a3feef6af23e2799c7d42e3c316c8e340898
Kekee <kekekuza@gmail.com>
2022-07-29 09:30:46 +0300

    Theme changes for user page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\MshuleUserRepository;
+use App\Service\PopulatePageData;
+use Bridge\Src\Helpers\MenuInfo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,17 +14,24 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('security/user_view.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
 
     #[Route('employee-header', name: 'app_employee_header')]
-    public  function EmployeeHeader(MshuleUserRepository $repository)
+    public function EmployeeHeader(MshuleUserRepository $repository)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-            'emp'=>$repository ->findAll()
-        ]);
+        return $this->render('security/user_view.html.twig',
+            (new PopulatePageData(
+                UserController::class,
+                MenuInfo::MENU_SYSTEM_SETTINGS,
+                true,
+                [
+                    'controller_name' => 'UserController',
+                    'emp' => $repository->findAll()
+                ]
+            ))->get()
+        );
     }
 }
